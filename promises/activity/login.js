8476cc4e94edcabe308069ea189cf344584b6571
Jayant <jayantkumar9181835@gmail.com>
2023-05-07 00:07:30 +0530

    for all challenge data

--- promises/activity/login.js
+++ promises/activity/login.js
@@ -14,4 +14,28 @@ const password = "Mar@2020";
     await tab.type("#input-1", id); 
     await tab.type("#input-2", password); 
     await tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+   //await tab.click(".user-avatar-placeholder.d-flex.align-items-center.justify-content-center");
+
+   await  tab.waitForSelector('div[data-analytics="NavBarProfileDropDown"]',{visible:true});
+   await tab.click('div[data-analytics="NavBarProfileDropDown"]');
+   await  tab.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]',{visible:true});
+   await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]');
+   await  tab.waitForSelector(".nav-tabs.nav.admin-tabbed-nav li");
+   let bothtags = await tab.$$(".nav-tabs.nav.admin-tabbed-nav li");
+   let managetag= bothtags[1];
+   await managetag.click();
+
+   await  tab.waitForSelector(".btn.btn-green.backbone.pull-right",{visible:true});
+   let createtags = await tab.$(".btn.btn-green.backbone.pull-right") ;
+   let createChallengeLink = await tab.evaluate( function(elem){ return elem.getAttribute("href");  }  ,  createtags);
+    createChallengeLink = 'https://www.hackerrank.com'+createChallengeLink;
+
+    await addChallenge(challenges[0] , browser , createChallengeLink);
 })();
+
+
+async function addChallenge(challenge , browser , createChallengeLink ){
+
+
+    
+}
