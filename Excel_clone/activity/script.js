94f71a2b2fd5d66419ccd3a759b5525035d03dce
Jayant <jayantkumar9181835@gmail.com>
2023-06-05 12:06:59 +0530

    cell posiioning

--- Excel_clone/activity/script.js
+++ Excel_clone/activity/script.js
@@ -1,16 +1,48 @@
 let cellcontent = document.querySelector(".cells-content");
 
 function initcells(){
-let cells ="";
-for(let i=0;i<100;i++){
-    cells += "<div class = 'row'>"
-    for(let j=0 ; j<26 ; j++){
-        cells += "<div class = 'cell'>Cell</div>"
-    }
-    cells += "</div>"
+let cellscontent ="<div class = 'top-left-cell'></div>";
+cellscontent += "<div class = 'top-row'>"
+
+for(let i=0;i<26;i++){
+ cellscontent += `<div class= 'top-row-cell'>${String.fromCharCode(65+i)}</div>`
 }
-cellcontent.innerHTML = cells;
+cellscontent +="</div>"
+
+cellscontent += "<div class = 'left-col'>"
+
+for(let i=0;i<100;i++){
+    cellscontent += `<div class='left-col-cell'>${i+1}</div>`;
+
+}
+
+cellscontent +="</div>"
+cellscontent +="<div class = 'cells'  >"
+
+for(let i=0;i<100;i++){
+    cellscontent += "<div class = 'row'>"
+    for(let j=0 ; j<26 ; j++){
+        cellscontent += "<div class = 'cell' contentEditable = 'true' ></div>"
+    }
+    cellscontent += "</div>"
+}
+cellscontent +="</div>"
+cellcontent.innerHTML = cellscontent;
  
 }
 
+
 initcells();
+
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
+let topLeftCell = document.querySelector(".top-left-cell");
+
+cellcontent.addEventListener("scroll" , function(e){
+    let top = e.target.scrollTop;
+    let left = e.target.scrollLeft;
+    topRow.style.top = top + "px";
+    topLeftCell.style.top = top + "px";
+    topLeftCell.style.left = left + "px";
+    leftCol.style.left = left + "px";  
+})
