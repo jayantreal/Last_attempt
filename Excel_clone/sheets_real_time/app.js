3d03a6cdc05ce6e15f13fbabe54cbf2c18a946ec
Jayant <jayantkumar9181835@gmail.com>
2023-07-05 13:29:22 +0530

    real time attempt

---
+++ Excel_clone/sheets_real_time/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { Server } = require("socket.io");
+// server is created !!!
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+//app.use(express.json());
+app.use(express.static("public"));
+
+let userList = []; // for online list
+
+// connection event is attached on io
+io.on("connection" , function(socket){ 
+    console.log(socket.id + " connected !!!");
+
+   
+})
+
+
+// tcp port 5500
+server.listen(5500 , function(){
+    console.log("Server started at port 5500 !!!");
+})
